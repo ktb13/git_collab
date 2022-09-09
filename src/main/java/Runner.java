78cb2c516792af99d477afe7950b4153a33f9c2c
ktb13 <ktbhayat13@gmail.com>
2022-09-09 11:56:11 +0100

    sout develop

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,6 +1,7 @@
 public class Runner {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world!");
+        System.out.println("we're working on develop");
     }
 }
